a100921ece3a3ebf3b2cdd3cb19cc5589f36542b
asroor <asror.style@gmail.com>
2023-10-09 09:34:29 +0300

    Express js app.PUT

--- index.js
+++ index.js
@@ -11,17 +11,11 @@ const users = [];
 const minName = 3;
 // User larni response qilib yuborish
 app.get('/users', (req, res) => {
-    res.send(users);
+    res.status(200).send(users);
 });
-// User-ni Validatsiya qilish uchun sxema || Joi package
-userSchema = Joi.object({
-    name: Joi.string().min(minName).required(),
-    stack: Joi.string().required()
-})
-
 // Yangi User qo'shish
 app.post('/users', (req, res) => {
-    const { error, value } = userSchema.validate(req.body);
+    const { error, value } = validateUser(req.body);
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
@@ -34,13 +28,45 @@ app.post('/users', (req, res) => {
     res.status(201).send(user);
 });
 
+// User ma'lumotlarini yangilash
+app.put('/users/:id', (req, res) => {
+    // User-ni id orqali topish
+    const user = users.find(u => u.id === req.params.id);
+    // User topilmasa 404 error qaytarish
+    if (!user) {
+        return res.status(404).send('User topilmadi');
+    }
+    // User topilsa so'rovni validatsiya qilish
+    const { error, value } = validateUser(req.body);
+    // User validatsiyadan o'tmasa, 400 bad req qaytarish
+    if (error) {
+        res.status(400).send(error.details[0].message);
+    }
+    // User-ni yangilash
+    user.name = req.body.name;
+    user.stack = req.body.stack;
+    // Yangilangan User-ni qaytarish(res)
+    res.status(200).send(user);
+})
+
 // User-ni ID orqali topish
 app.get('/users/:id', (req, res) => {
     const user = users.find(u => u.id === req.params.id);
     if (!user) {
-        res.send("User topilmadi")
+        return res.status(404).send('User topilmadi')
     }
-    res.send(user)
-})
+    res.status(200).send(user);
+});
+
+
+// User-ni validate qilish funksiyasi
+function validateUser(user) {
+    const userSchema = Joi.object({
+        name: Joi.string().min(minName).required(),
+        stack: Joi.string().required()
+    });
+    return userSchema.validate(user);
+}
+
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server ${PORT}`))
